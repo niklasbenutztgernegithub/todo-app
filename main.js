a101faf5a152a3ad6b35c2a1f8ad005fee961e38
Niklas Herz <niklas.herz@neuwaerts.de>
2023-06-07 13:59:12 +0200

    +ability to add new tasks to board

---
+++ main.js
@@ -0,0 +1,22 @@
+let testinput = document.querySelector(".checklist-item-input")
+let textinput
+let output = document.getElementById("test-output")
+const addItemButton = document.getElementById("AddItemButton")
+let checklistItem = (text) => {
+   const parser = new DOMParser();
+   const {body} = parser.parseFromString(`<section class="checklist-item">
+   <p class= "checklist-item-text">`+ text + `</p>
+   <button class="checkbox done"></button>
+   <button class="checkbox delete"></button>   
+ </section>`, "text/html")
+    return body
+}
+const checkliste = document.querySelector(".checkliste")
+testinput.addEventListener("input", () => {
+    textinput = testinput.value
+})
+
+addItemButton.addEventListener("click", (event) => {
+    console.log(textinput)
+   if (textinput) {checkliste.prepend(checklistItem(textinput))}
+})
