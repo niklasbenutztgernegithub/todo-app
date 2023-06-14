e83462f08e83ea62c96799cdd754c3bc65fd283f
Niklas Herz <niklas.herz@neuwaerts.de>
2023-06-14 10:25:50 +0200

    +wow such empty, + actually removes stuff on demand

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-let testinput = document.querySelector(".checklist-item-input")
-let textinput
-let output = document.getElementById("test-output")
+let input = document.querySelector(".checklist-item-input")
+let textinput;
 const addItemButton = document.getElementById("AddItemButton")
+const checklistDone = document.getElementById("checklist-done")
+const checklistEmpty = document.querySelector(".checklist-item.empty")
+let wowSuchEmpty = () => {
+    if (checklistDone.children.length > 0) {
+checklistEmpty.style.display = "none";
+
+    } else {
+        checklistEmpty.removeAttribute("style")
+    };
+};
 let checklistItem = (text) => {
-   const parser = new DOMParser();
-   const {body} = parser.parseFromString(`<section class="checklist-item">
+    const parser = new DOMParser();
+    const { body } = parser.parseFromString(`<section class="checklist-item">
    <p class= "checklist-item-text">`+ text + `</p>
    <button class="checkbox done"></button>
    <button class="checkbox delete"></button>   
@@ -12,16 +21,28 @@ let checklistItem = (text) => {
     const element = body.querySelector('body>*');
     let removeButton = element.querySelector(".checkbox.delete")
     removeButton.addEventListener("click", () => {
+        if (confirm("Möchtest du folgende Notiz löschen?: " + text)) {
+            element.remove()
+            wowSuchEmpty()
+        }
+        
+    }) // destroys and recreates the element
+    let doneButton = element.querySelector(".checkbox.done")
+    doneButton.addEventListener("click", () => {
         element.remove()
+        element.classList.add("undo")
+        checklistDone.prepend(element)
+        wowSuchEmpty()
+
     })
     return element;
 }
-const checkliste = document.querySelector(".checkliste")
-testinput.addEventListener("input", () => {
-    textinput = testinput.value
+
+const checkliste = document.querySelector(".checklist-todo")
+input.addEventListener("input", () => {
+    textinput = input.value
+})
+addItemButton.addEventListener("click", (event) => {
+    if (textinput) { checkliste.prepend(checklistItem(textinput)) }
 })
 
-addItemButton.addEventListener("click", (event) => {
-    console.log(textinput)
-   if (textinput) {checkliste.prepend(checklistItem(textinput))}
-})
