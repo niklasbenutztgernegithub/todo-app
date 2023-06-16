21762ac99a3d474e907df991b29d448eebf4f9cd
Niklas Herz <niklas.herz@neuwaerts.de>
2023-06-16 14:27:50 +0200

    all nice and clean :)

--- main.js
+++ main.js
@@ -7,9 +7,13 @@ let textinput;
 
 // Read and write JSON objects to localStorage
 const local = (key, value = null) => {
+    //if value parameter is set, write value to local storage
     if (value != null) return localStorage[key] = JSON.stringify(value);
+
+    //return local storage key values if set
     return localStorage?.[key] ? JSON.parse(localStorage[key]) : null;
 };
+
 
 const checklistItem = (text, time = Date.now()) => {
     const parser = new DOMParser();
@@ -20,7 +24,7 @@ const checklistItem = (text, time = Date.now()) => {
  </section>`, "text/html");
     const element = body.querySelector('body>*');
 
-        // remove the checklist element 
+    // remove the checklist element on click
 
     let removeButton = element.querySelector(".checkbox.delete");
     removeButton.addEventListener("click", () => {
@@ -34,7 +38,6 @@ const checklistItem = (text, time = Date.now()) => {
         }
     });
 
-
     // destroys and recreates the element
 
     let doneButton = element.querySelector(".checkbox.done");
@@ -47,8 +50,18 @@ const checklistItem = (text, time = Date.now()) => {
     return element;
 };
 
+// hides/shows the "wow such empty" container if no items are present
 
+    const wowSuchEmpty = () => {
+        if (checklistDone.children.length > 0) {
+            checklistEmpty.style.display = "none";
+        } else { checklistEmpty.removeAttribute("style"); }
+    };
+    
+// Creates a new local storage component when there isnt one
 if (!local("toDos")) local("toDos", []);
+
+// updates the checklist if there are elements found in storage
 if (local("toDos")) {
   for (const toDo of local("toDos")) {
     checkliste.prepend(checklistItem(toDo.text, toDo.time));
@@ -56,35 +69,29 @@ if (local("toDos")) {
   }
 }
 
-
-// hides/shows the "wow such empty" container
-
-const wowSuchEmpty = () => {
-    if (checklistDone.children.length > 0) {
-        checklistEmpty.style.display = "none";
-    } else { checklistEmpty.removeAttribute("style"); }
-};
-
-
-// generates a checklist item 
-
-
+// gets the input value
 input.addEventListener("input", () => {
     textinput = input.value;
 });
 
+// handles the logic to add an item to the checklist
 addItemButton.addEventListener("click", (event) => {
     if (textinput) {
+
+        //adds the text from input into a new checklistItem
         const item = checklistItem(textinput)
         checkliste.prepend(item);
+
+        // saves the textinput and time stamp into local storage
         let array = local('toDos');
         array.push({
             text: textinput,
             time: item.id
         });
         local('toDos', array);
+        
+        // resets the input field
         input.value = null;
     }
 
 });
-
